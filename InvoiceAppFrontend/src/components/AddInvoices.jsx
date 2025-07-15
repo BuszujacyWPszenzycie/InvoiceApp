cc539326fad02ca4ffdc476ffc9bd0b864acd1e2
Marcin <marcin.szturminski@gmail.com>
2025-07-15 18:36:29 +0200

    added option to add invoice to database

--- InvoiceAppFrontend/src/components/AddInvoices.jsx
+++ InvoiceAppFrontend/src/components/AddInvoices.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import InputAmount from './form/InputAmount'
 import InputDate from './form/InputDate'
 import Button from './ui/Button'
-import './AddInvoices.scss'
 import axios from 'axios'
+import './AddInvoices.scss'
 
 function AddInvoice() {
+	const navigate = useNavigate()
+
 	const [form, setForm] = useState({
 		number: '',
 		date: '',
@@ -23,11 +26,17 @@ function AddInvoice() {
 			.post('http://localhost:4000/api/invoices', form)
 			.then(res => {
 				console.log('✔️ Faktura zapisana:', res.data)
-				// TODO: Możesz tu przekierować lub pokazać komunikat
+				// Po sukcesie przejdź do listy faktur
+				navigate('/dashboard/invoices')
 			})
 			.catch(err => {
 				console.error('❌ Błąd zapisu faktury:', err)
+				// Możesz tu dodać obsługę błędu, np. komunikat dla użytkownika
 			})
+	}
+
+	const handleCancel = () => {
+		navigate('/dashboard') // lub '/dashboard/invoices', jak wolisz
 	}
 
 	return (
@@ -44,7 +53,9 @@ function AddInvoice() {
 				</div>
 
 				<div className='add-invoice__actions'>
-					<Button variant='danger'>Anuluj</Button>
+					<Button variant='danger' type='button' onClick={handleCancel}>
+						Anuluj
+					</Button>
 					<Button type='submit' variant='primary'>
 						Zapisz fakturę
 					</Button>
